f7eeed5424e2322283798adde892920f547a8f68
Jonas Platte <jplatte+git@posteo.de>
2017-10-20 20:49:23 +0200

    Use the official name

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 extern crate rocket;
 extern crate rocket_contrib;
 
-mod turbo_fish;
+mod turbofish;
 
 use std::collections::HashMap;
 use std::path::{Path, PathBuf};
@@ -12,22 +12,22 @@ use std::path::{Path, PathBuf};
 use rocket::response::NamedFile;
 use rocket_contrib::Template;
 
-use turbo_fish::TurboFish;
+use turbofish::TurboFish;
 
 #[get("/")]
 fn index() -> Template {
     let mut context = HashMap::new();
     context.insert("guts", "");
 
-    Template::render("turbo_fish", context)
+    Template::render("turbofish", context)
 }
 
-#[get("/<turbo_fish>")]
-fn turbo_fish(turbo_fish: TurboFish) -> Template {
+#[get("/<turbofish>")]
+fn turbofish(turbofish: TurboFish) -> Template {
     let mut context = HashMap::new();
-    context.insert("guts", turbo_fish.gut());
+    context.insert("guts", turbofish.gut());
 
-    Template::render("turbo_fish", context)
+    Template::render("turbofish", context)
 }
 
 // From https://github.com/SergioBenitez/Rocket/blob/master/examples/static_files/src/main.rs
@@ -39,7 +39,7 @@ fn files(file: PathBuf) -> Option<NamedFile> {
 fn main() {
     // TODO: Custom 404
     rocket::ignite()
-        .mount("/", routes![index, turbo_fish, files])
+        .mount("/", routes![index, turbofish, files])
         .attach(Template::fairing())
         .launch();
 }
