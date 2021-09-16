8dfa4e28e2745db006c279b51b0d7b9a392c2adf
Konrad Borowski <konrad@borowski.pw>
2021-09-16 13:01:40 +0200

    Remove dynamic dispatch in random type generator
    It's replaced with variable length slices which don't necessitate
    an implementation for each possible size.

--- src/random.rs
+++ src/random.rs
@@ -1,83 +1,50 @@
-use rand::seq::SliceRandom;
+use itertools::Itertools;
+use rand::{prelude::ThreadRng, seq::SliceRandom};
+use std::borrow::Cow;
 
 const RECURSION_LIMIT: u8 = 1;
 
-trait Type: Send + Sync {
-    fn num_generic_params(&self) -> usize;
-    fn stringify(&self, generic_params: &[String]) -> String;
-}
-
-impl Type for &'static str {
-    fn num_generic_params(&self) -> usize {
-        0
-    }
-
-    fn stringify(&self, _generic_params: &[String]) -> String {
-        (*self).to_owned()
-    }
-}
-
-impl Type for (&'static str, &'static str) {
-    fn num_generic_params(&self) -> usize {
-        1
-    }
-
-    fn stringify(&self, generic_params: &[String]) -> String {
-        format!("{}{}{}", self.0, generic_params[0], self.1)
-    }
-}
-
-impl Type for (&'static str, &'static str, &'static str) {
-    fn num_generic_params(&self) -> usize {
-        2
-    }
-
-    fn stringify(&self, generic_params: &[String]) -> String {
-        format!("{}{}{}{}{}", self.0, generic_params[0], self.1, generic_params[1], self.2)
-    }
-}
-
-static TYPES: &[&dyn Type] = &[
-    &"_",
-    &"bool",
-    &"char",
-    &"i8",
-    &"i16",
-    &"i32",
-    &"i64",
-    &"isize",
-    &"u8",
-    &"u16",
-    &"u32",
-    &"u64",
-    &"usize",
-    &"f32",
-    &"f64",
-    &"&str",
-    &"String",
-    &"()",
-    &("&", ""),
-    &("&mut ", ""),
-    &("[", "]"),
-    &("Box<", ">"),
-    &("Vec<", ">"),
-    &("HashSet<", ">"),
-    &("Result<", ", ", ">"),
-    &("HashMap<", ", ", ">"),
+const TYPES: &[&[&str]] = &[
+    &["_"],
+    &["bool"],
+    &["char"],
+    &["i8"],
+    &["i16"],
+    &["i32"],
+    &["i64"],
+    &["isize"],
+    &["u8"],
+    &["u16"],
+    &["u32"],
+    &["u64"],
+    &["usize"],
+    &["f32"],
+    &["f64"],
+    &["&str"],
+    &["String"],
+    &["()"],
+    &["&", ""],
+    &["&mut ", ""],
+    &["[", "]"],
+    &["Box<", ">"],
+    &["Vec<", ">"],
+    &["HashSet<", ">"],
+    &["Result<", ", ", ">"],
+    &["HashMap<", ", ", ">"],
 ];
 
 pub fn random_type() -> String {
-    random_type_depth(0)
+    random_type_depth(0, &mut rand::thread_rng())
 }
 
-fn random_type_depth(depth: u8) -> String {
-    let ty = TYPES.choose(&mut rand::thread_rng()).unwrap();
-
-    if depth == RECURSION_LIMIT {
-        ty.stringify(&(0..ty.num_generic_params()).map(|_| "_".to_owned()).collect::<Vec<_>>())
-    } else {
-        ty.stringify(
-            &(0..ty.num_generic_params()).map(|_| random_type_depth(depth + 1)).collect::<Vec<_>>(),
-        )
-    }
+fn random_type_depth(depth: u8, rng: &mut ThreadRng) -> String {
+    let &ty = TYPES.choose(rng).unwrap();
+    Itertools::intersperse_with(ty.iter().map(|&x| Cow::Borrowed(x)), || {
+        if depth == RECURSION_LIMIT {
+            "_".into()
+        } else {
+            random_type_depth(depth + 1, rng).into()
+        }
+    })
+    .collect()
 }
